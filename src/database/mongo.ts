6b009e5ad11e17986578d15c6e498c149851946b
mdhaafil <mohammedhaafil5@gmail.com>
2026-05-13 12:19:50 +0530

    fix mongodb atlas

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,8 +1,19 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb://127.0.0.1:27017/ssdb");
+//     console.log("✅ MongoDB Connected");
+//   } catch (error) {
+//     console.error("❌ MongoDB connection failed", error);
+//     process.exit(1);
+//   }
+// };
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/ssdb");
+    await mongoose.connect(process.env.MONGO_URI as string);
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB connection failed", error);
